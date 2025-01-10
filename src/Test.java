9cc9b329990c4dc644f5952efe7254522d6daa1f
BT_1N3_04 <leoz32@nycstudents.net>
2025-01-10 10:51:18 -0500

    initial2

--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,7 @@
 public class Test {
     public static void main(String[] args) {
-        System.out.println("leo");
-
+        System.out.println("our first shared repo");
+        System.out.println("this is the other partner adding some code");
+        System.out.println("what's up");
     }
 }
